776fc9e135f245c6e8b871c1a8059eb547c3251f
finnmo <finn.morris00@gmail.com>
2024-07-04 05:38:41 +0000

    Deploying to gh-pages from @ optech-curtin/shelly-ips@75752d6217f9cf849943eb7c99565e18437a3350 🚀

--- _next/static/dk6NDNIOdc-RRQIIF7Hyr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/5aeb7938e8507bbe.css","static/chunks/pages/index-9a7e96dfd0cc5cef.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/x00qaXXgFsBF68_YU-OJN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/5aeb7938e8507bbe.css","static/chunks/pages/index-8168083505131d1f.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
