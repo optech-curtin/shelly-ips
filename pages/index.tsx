ce6e3f541c2f361936f90e273c599a76c35c9c7f
Finn Morris <finn.morris@curtin.edu.au>
2025-03-21 08:00:00 +0800

    Updated IP and added embedding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,43 @@
-import type { NextPage } from 'next'
+// /pages/index.tsx
+
+import { NextPage } from 'next'
+import Link from 'next/link'
 import styles from '../styles/styles.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-      <h1 className={styles.title}>
-          Shelly Ip Address Table
-        </h1>
+
+        <h1 className={styles.title}>Shelly IP Address Table</h1>
+
         <div className={styles.grid}>
-          <a href="http://10.129.250.138" className={styles.card}>
+
+          {/* Use a Next.js Link to pass the URL as a query param */}
+          <Link
+            href={`/embedded?url=${encodeURIComponent('http://10.129.250.138')}`}
+            className={styles.card}
+          >
             <h2>POTRAT Room &rarr;</h2>
             <p>Shelly Pro 3</p>
-          </a>
+          </Link>
 
-          <a href="http://10.129.250.84" className={styles.card}>
+          <Link
+            href={`/embedded?url=${encodeURIComponent('http://10.130.209.17/')}`}
+            className={styles.card}
+          >
             <h2>CCTV Trailer &rarr;</h2>
             <p>Shelly Pro 2</p>
-          </a>
+          </Link>
+
         </div>
       </main>
 
       <footer className={styles.footer}>
-          <p>Contact <a href="mailto:operationaltechnology@curtin.edu.au">Operational Technology</a> for more Information</p>
+        <p>
+          Contact <a href="mailto:operationaltechnology@curtin.edu.au">Operational Technology</a> 
+          for more Information
+        </p>
       </footer>
     </div>
   )
